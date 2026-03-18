c108188dc4e65689333228d68718a587dcd45a41
aksab-git <finance@aksab.shop>
2026-03-18 20:46:42 +0000

    build: reset build directory to default and fix path issues

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.google.gms:google-services:4.4.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
 
@@ -15,4 +16,10 @@ allprojects {
     }
 }
 
-// باقي الكود الخاص بـ Build Directory سليم كما هو
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
